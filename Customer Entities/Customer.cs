f35dee70ce983ec62ffaa4228b720c833104a049
Thomas <baloyi.thomas@outlook.com>
2020-11-15 23:10:45 +0200

    Added getters and setters in the Customer class.

--- Customer Entities/Customer.cs
+++ Customer Entities/Customer.cs
@@ -8,5 +8,83 @@ namespace Poppel_Ordering_System.Customer_Entities
 {
     class Customer
     {
+        #region Fields
+        private int customerID;
+        private string firstName;
+        private string lastName;
+        private string creditStatus;
+        #endregion
+
+        #region Construtors
+        public Customer()
+        {
+            customerID = 0;
+            firstName = null;
+            lastName = null;
+            creditStatus = null;
+        }
+
+        public Customer(int cID, string fName, string lName, string cStatus)
+        {
+            customerID = cID;
+            firstName = fName;
+            lastName = lName;
+            creditStatus = cStatus;
+        }
+        #endregion
+
+        #region Properties
+        public int CustomerID
+        {
+            get
+            {
+                return customerID;
+            }
+
+            set
+            {
+                customerID = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string CreditStatus
+        {
+            get
+            {
+                return creditStatus;
+            }
+
+            set
+            {
+                creditStatus = value;
+            }
+        }
+        #endregion
     }
 }
