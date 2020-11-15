2deba4ae136ef61cd0bce112d2dd95bce5045dbd
Thomas <baloyi.thomas@outlook.com>
2020-11-15 23:50:18 +0200

    Changed the colour to blue.

--- Presentation Layer/.Designer.cs
+++ Presentation Layer/.Designer.cs
@@ -93,6 +93,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.welcomePoppelLabel);
             this.Controls.Add(this.cancelItemButton);
