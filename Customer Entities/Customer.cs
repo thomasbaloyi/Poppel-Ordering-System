fb70528024332543daf9df2295d365725699cfba
Thomas <baloyi.thomas@outlook.com>
2020-11-15 22:50:49 +0200

    Added folders: Presentation Layer, Database Layer, & Customer Entities. Created: Customer class.

---
+++ Customer Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poppel_Ordering_System.Customer_Entities
+{
+    class Customer
+    {
+    }
+}
